632725c6f49375b28f1b1f2c98418133cd1fbaf8
olov <olov@16daf88e690b>
2019-05-27 05:59:55 +0000

    more work on using ServiceLoader to load AppTokenStorage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module se.uu.ub.cora.gatekeepertokenprovider {
 	requires transitive se.uu.ub.cora.httphandler;
 	requires se.uu.ub.cora.json;
+	requires se.uu.ub.cora.logger;
 
 	exports se.uu.ub.cora.gatekeepertokenprovider;
 	exports se.uu.ub.cora.gatekeepertokenprovider.authentication;
